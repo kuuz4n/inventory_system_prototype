1a7a4c00eb6532aff4a436de85f03c51dc817dc9
Kevin <kevinroicanete@gmail.com>
2020-01-16 23:47:00 +0800

    Added Search Box

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,6 +41,10 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -131,7 +135,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(65, 301);
+            this.button1.Location = new System.Drawing.Point(24, 301);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 57);
             this.button1.TabIndex = 10;
@@ -141,28 +145,73 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(166, 301);
+            this.button2.Location = new System.Drawing.Point(128, 301);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 57);
             this.button2.TabIndex = 11;
             this.button2.Text = "Remove Item";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(351, 12);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(469, 457);
             this.dataGridView1.TabIndex = 12;
             this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(229, 301);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 57);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Update Item";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 26);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(40, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 31);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Search:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(177, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 25);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(953, 520);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -200,6 +249,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -74,10 +74,56 @@ namespace InventoryPrototype
 
         private void dataGridView1_MouseDoubleClick_1(object sender, MouseEventArgs e)
         {
-            txt_item_code.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
-            txt_item_name.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
-            txt_price.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
-            txt_stocks.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txt_item_code.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            txt_item_name.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            txt_price.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            txt_stocks.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "UPDATE items SET item_name = '"+txt_item_name.Text+"', stocks = '"+txt_stocks.Text+"', item_code = '"+txt_item_code.Text+"',price = '"+txt_price.Text+"'";
+            MySqlDataAdapter SDA = new MySqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Update Successfully!");
+            txt_item_code.Text = "";
+            txt_item_name.Text = "";
+            txt_price.Text = "";
+            txt_stocks.Text = "";
+            runQuery();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "DELETE FROM items where item_code = '" + txt_item_code.Text + "'";
+            MySqlDataAdapter SDA = new MySqlDataAdapter(query, con);
+            SDA.SelectCommand.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Successfully Removed!!");
+            runQuery();
+        }
+
+        public void searchData()
+        {
+            string query = "SELECT * FROM items WHERE CONCAT(`item_name`,`stocks`,`item_code`,`price`) like '%"+ textBox1.Text.ToString()+"%'";
+            MySqlDataAdapter SDA = new MySqlDataAdapter(query, con);
+            DataTable table = new DataTable();
+            SDA.Fill(table);
+            dataGridView1.DataSource = table;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            searchData();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            searchData();
         }
     }
 }
